17656182b8982854424750ac9cb4a01bc8a50a3f
Vitaliy Smorshchok <turnik099@gmail.com>
2022-01-03 14:26:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const numberOfFilms = +prompt(
+    'How many films watch?', ''
+);
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Last watch film?', ''),
+    b = promt('rank of number', ''),
+    c = prompt('Last watch film?', ''),
+    d = prompt('Last watch film?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
